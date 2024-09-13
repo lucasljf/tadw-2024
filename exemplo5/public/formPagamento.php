76f11c745d159c3c50d2845f1720641fb38b1994
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2024-09-13 11:19:24 -0300

    Views: emprestimo e pagamento
    Ainda incompleto.

--- exemplo5/public/formPagamento.php
+++ exemplo5/public/formPagamento.php
@@ -1,22 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h2>Lançar pagamento</h2>
     <form action="">
         <input type="hidden" value="<?php echo $_GET['idemprestimo']; ?>">
         Valor: <br>
+        <input type="text" name="valor"><br>
         Preço por KM: <br>
+        <input type="text" name="precokm"><br>
         Método pagamento: <br>
+        <input type="text" name="metodo"><br>
 
         Carros: <br>
         <?php
-            // $carros = listar
+        require_once "../conexao.php";
+        require_once "../operacoes.php";
+
+        $carros = listarVeiculosEmprestimo($conexao, $_GET['idemprestimo']);
+
+        foreach ($carros as $carro) {
+            
+            echo "<hr><hr>";
+            echo "<input type='hidden' value='$carro[0]'>";
+            echo $carro[1];
+        }
         ?>
     </form>
 </body>
+
 </html>
