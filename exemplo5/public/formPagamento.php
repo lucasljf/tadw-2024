44042427eb99e959ac463d4c69a3112a078d3f29
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2024-09-19 16:20:51 -0300

    Update formPagamento.php

--- exemplo5/public/formPagamento.php
+++ exemplo5/public/formPagamento.php
@@ -11,27 +11,38 @@
     <h2>Lançar pagamento</h2>
     <form action="">
         <input type="hidden" value="<?php echo $_GET['idemprestimo']; ?>">
-        Valor: <br>
-        <input type="text" name="valor"><br>
         Preço por KM: <br>
-        <input type="text" name="precokm"><br>
+        <input type="text" name="precokm" id="precokm"><br>
         Método pagamento: <br>
-        <input type="text" name="metodo"><br>
-
-        Carros: <br>
+        <!-- <input type="text" name="metodo"><br> -->
+        <select name="" id="">
+            <option>Dinheiro</option>
+            <option>Cartão</option>
+        </select> <br>
+        
+        <h4>Carros</h4>
+        <hr>
         <?php
         require_once "../conexao.php";
         require_once "../operacoes.php";
-
+        
         $carros = listarVeiculosEmprestimo($conexao, $_GET['idemprestimo']);
-
-        foreach ($carros as $carro) {
-            
-            echo "<hr><hr>";
-            echo "<input type='hidden' value='$carro[0]'>";
-            echo $carro[1];
+        
+        // echo "<hr><hr>";
+        foreach ($carros as $carroEmprestimo) {
+            $veiculo = listarVeiculoPorId($conexao, $carroEmprestimo[0]);
+            echo "<input type='hidden' value='$veiculo[0]'>";
+            echo "<p>Veículo: $veiculo[2] - $veiculo[3]</p>";
+            echo "<p>Km Inicial: $veiculo[1]</p>";
+            echo "Km Final: <input type='text' name='kmfinal[]'>";
+            echo "<hr>";
         }
         ?>
+        Valor total a pagar: <br>
+        <input type="text" name="valor" disabled><br>
+
+        <button onclick="">Calcular valor</button>
+        <input type="submit" value='Lançar pagamento'>
     </form>
 </body>
 
